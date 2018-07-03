c81f7001c0c2befb8cbd8c40a38873af27a6dff2
sunwentao <15234546594@163.com>
2018-07-03 18:38:17 +0800

    你好

--- jishi/app1/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserInfo(models.Model):
-    uname = models.CharField(max_length=20) # 姓名
-    upwd = models.CharField(max_length=60) # 密码
-    uemail = models.EmailField(max_length=40, default='无') # 邮箱
-    uaddree = models.CharField(max_length=100, default='无') # 地址
-    uliuyan = models.CharField(max_length=200, default='无') # 留言
--- lecongpro/userapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from userapp.models import *
-admin.site.register(UserInfo)
